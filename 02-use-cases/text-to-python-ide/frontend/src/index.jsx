6fffc6aec2909ec566664758753fdef8416ab301
nabaws <nmurich@amazon.com>
2025-09-04 16:42:25 -0400

    fix(text-to-python-ide): updated to remove vulnerable react components (#297)

---
+++ 02-use-cases/text-to-python-ide/frontend/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@cloudscape-design/global-styles/index.css';
+import App from './App.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
